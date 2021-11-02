b485280299f0c6a10430dc8eefec928e2b9eb58a
aristotelesjunior <Tenchu88*>
2021-11-02 14:28:29 -0300

    Criação da view listagem

--- contas/views.py
+++ contas/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from models import Transacao
+from .models import Transacao
 import datetime
 
 
@@ -10,4 +10,11 @@ def home(request):
     data['now'] = datetime.datetime.now()
     data['transacoes'] = ['t1', 't2', 't3']
     # html = "<html><body>Agora são %s.</body></html>" % now
+
     return render(request=request, template_name="contas/home.html", context=data)
+
+
+def listagem(request):
+    data = {}
+    data['transacoes'] = Transacao.objects.all()
+    return render(request=request, template_name="contas/listagem.html", context=data)
